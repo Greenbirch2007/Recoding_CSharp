78542f07d16cd6aaaf2608d02e994d8def03ad35
Greenbirch2007 <291109028@qq.com>
2019-12-29 14:23:30 +0800

    C# 类型转换

--- cst/cs1.cs
+++ cst/cs1.cs
@@ -1,19 +1,14 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text.ReqularExpressions;
+
+
 
 namespace Rextester
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            Console.WriteLine("Hello, world!");
-        }
+        Console.WriteLine("Hello world!")；
     }
 }
